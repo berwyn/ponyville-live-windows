7bb0a183c5be9375781edb730c40fdc3f876fcab
berwyn <berwyn.codeweaver@gmail.com>
2014-11-06 18:32:08 -0700

    Basic app generation and bindings

---
+++ PVL/PVL.Shared/DataModel/StationGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PVL.DataModel
+{
+    class StationGroup
+    {
+
+        public string Title { get; set; }
+        public ObservableCollection<Station> Stations { get; set; }
+
+    }
+}
---
+++ pvl.cs/API.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Newtonsoft.Json;
+using PortableRest;
+
+namespace PVL
+{
+    public class Error
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ArrayResponse<T>
+    {
+        public string Status { get; set; }
+        public T[] Result { get; set; }
+    }
+
+    public class Station
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        [JsonProperty("image_url")]
+        public string ImageURL { get; set; }
+    }
+
+    public class API : RestClient
+    {
+        private static readonly string API_BASE = "http://ponyvillelive.com/api";
+
+        private static API _instance = new API();
+
+        public static API Instance
+        {
+            get { return _instance; }
+        }
+
+        public API()
+        {
+            BaseUrl = API_BASE;
+        }
+
+        public async Task<Station[]> StationFetchTask()
+        {
+            var request = new RestRequest("/station/list");
+            var response = await SendAsync<ArrayResponse<Station>>(request);
+            return response.Content.Result;
+        }
+
+        public async Task<Station[]> StationFetchTask(string category)
+        {
+            var request = new RestRequest("/station/list/category/{category}");
+            request.AddUrlSegment("category", category);
+            
+            var response = await SendAsync<ArrayResponse<Station>>(request);
+            return response.Content.Result;
+        }
+    }
+}
