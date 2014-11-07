d71e07ae6cbad8c19b18ebb110775d28ff7b449a
berwyn <berwyn.codeweaver@gmail.com>
2014-11-06 22:24:58 -0700

    Images

---
+++ PVL/PVL.Shared/Common/Converters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace PVL.Common
+{
+
+    public sealed class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value is bool && (bool) value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is Visibility && (Visibility) value == Visibility.Visible;
+        }
+    }
+
+    public sealed class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return (value is bool && (bool)value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return value is Visibility && (Visibility)value == Visibility.Collapsed;
+        }
+    }
+
+}
--- PVL/PVL.Windows/HubPage.xaml.cs
+++ PVL/PVL.Windows/HubPage.xaml.cs
@@ -1,59 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// The Universal Hub Application project template is documented at http://go.microsoft.com/fwlink/?LinkID=391955
+using System;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel.Resources;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Media;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using PVL.Data;
 using PVL.Common;
-
-// The Universal Hub Application project template is documented at http://go.microsoft.com/fwlink/?LinkID=391955
+using PVL.Data;
 using PVL.DataModel;
 
 namespace PVL
 {
     /// <summary>
-    /// A page that displays a grouped collection of items.
+    ///     A page that displays a grouped collection of items.
     /// </summary>
     public sealed partial class HubPage : Page
     {
-        private NavigationHelper navigationHelper;
-        private ObservableDictionary defaultViewModel = new ObservableDictionary();
-        private SystemMediaTransportControls systemControls;
-
-        /// <summary>
-        /// Gets the NavigationHelper used to aid in navigation and process lifetime management.
-        /// </summary>
-        public NavigationHelper NavigationHelper
-        {
-            get { return this.navigationHelper; }
-        }
-
-        /// <summary>
-        /// Gets the DefaultViewModel. This can be changed to a strongly typed view model.
-        /// </summary>
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return this.defaultViewModel; }
-        }
+        private readonly ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private readonly NavigationHelper navigationHelper;
+        private readonly SystemMediaTransportControls systemControls;
+        private bool DataLoaded;
+        private bool _radioLoaded;
+        private bool _videoLoaded;
 
         public HubPage()
         {
-            this.InitializeComponent();
-            this.navigationHelper = new NavigationHelper(this);
-            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            InitializeComponent();
+            navigationHelper = new NavigationHelper(this);
+            navigationHelper.LoadState += NavigationHelper_LoadState;
 
             systemControls = SystemMediaTransportControls.GetForCurrentView();
             systemControls.ButtonPressed += SystemControls_ButtonPressed;
@@ -63,73 +39,92 @@ namespace PVL
         }
 
         /// <summary>
-        /// Populates the page with content passed during navigation.  Any saved state is also
-        /// provided when recreating a page from a prior session.
+        ///     Gets the NavigationHelper used to aid in navigation and process lifetime management.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return navigationHelper; }
+        }
+
+        /// <summary>
+        ///     Gets the DefaultViewModel. This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return defaultViewModel; }
+        }
+
+        /// <summary>
+        ///     Populates the page with content passed during navigation.  Any saved state is also
+        ///     provided when recreating a page from a prior session.
         /// </summary>
         /// <param name="sender">
-        /// The source of the event; typically <see cref="NavigationHelper"/>
+        ///     The source of the event; typically <see cref="NavigationHelper" />
         /// </param>
-        /// <param name="e">Event data that provides both the navigation parameter passed to
-        /// <see cref="Frame.Navigate(Type, object)"/> when this page was initially requested and
-        /// a dictionary of state preserved by this page during an earlier
-        /// session.  The state will be null the first time a page is visited.</param>
+        /// <param name="e">
+        ///     Event data that provides both the navigation parameter passed to
+        ///     <see cref="Frame.Navigate(Type, object)" /> when this page was initially requested and
+        ///     a dictionary of state preserved by this page during an earlier
+        ///     session.  The state will be null the first time a page is visited.
+        /// </param>
         private async void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
         {
-
             LoadAudioStations();
             LoadVideoStations();
         }
 
         /// <summary>
-        /// Invoked when a HubSection header is clicked.
+        ///     Invoked when a HubSection header is clicked.
         /// </summary>
         /// <param name="sender">The Hub that contains the HubSection whose header was clicked.</param>
         /// <param name="e">Event data that describes how the click was initiated.</param>
-        void Hub_SectionHeaderClick(object sender, HubSectionHeaderClickEventArgs e)
+        private void Hub_SectionHeaderClick(object sender, HubSectionHeaderClickEventArgs e)
         {
             HubSection section = e.Section;
-            var group = section.DataContext;
-            this.Frame.Navigate(typeof(SectionPage), ((SampleDataGroup)group).UniqueId);
+            object group = section.DataContext;
+            Frame.Navigate(typeof (SectionPage), ((SampleDataGroup) group).UniqueId);
         }
 
         /// <summary>
-        /// Invoked when an item within a section is clicked.
+        ///     Invoked when an item within a section is clicked.
         /// </summary>
-        /// <param name="sender">The GridView or ListView
-        /// displaying the item clicked.</param>
+        /// <param name="sender">
+        ///     The GridView or ListView
+        ///     displaying the item clicked.
+        /// </param>
         /// <param name="e">Event data that describes the item clicked.</param>
-        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+        private void ItemView_ItemClick(object sender, ItemClickEventArgs e)
         {
             try
             {
-                var streamUrl = ((Station) e.ClickedItem).StreamURL;
+                string streamUrl = ((Station) e.ClickedItem).StreamURL;
                 MediaPlayer.Source = new Uri(streamUrl);
-
             }
             catch (Exception ex)
             {
                 // TODO: Do something here to show error state
             }
         }
+
         #region NavigationHelper registration
 
         /// <summary>
-        /// The methods provided in this section are simply used to allow
-        /// NavigationHelper to respond to the page's navigation methods.
-        /// Page specific logic should be placed in event handlers for the  
-        /// <see cref="Common.NavigationHelper.LoadState"/>
-        /// and <see cref="Common.NavigationHelper.SaveState"/>.
-        /// The navigation parameter is available in the LoadState method 
-        /// in addition to page state preserved during an earlier session.
+        ///     The methods provided in this section are simply used to allow
+        ///     NavigationHelper to respond to the page's navigation methods.
+        ///     Page specific logic should be placed in event handlers for the
+        ///     <see cref="Common.NavigationHelper.LoadState" />
+        ///     and <see cref="Common.NavigationHelper.SaveState" />.
+        ///     The navigation parameter is available in the LoadState method
+        ///     in addition to page state preserved during an earlier session.
         /// </summary>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            this.navigationHelper.OnNavigatedTo(e);
+            navigationHelper.OnNavigatedTo(e);
         }
 
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
-            this.navigationHelper.OnNavigatedFrom(e);
+            navigationHelper.OnNavigatedFrom(e);
         }
 
         #endregion
@@ -137,28 +132,36 @@ namespace PVL
         #region Data Methods
 
         /// <summary>
-        /// Loads the audio stations from PVL and injects them into the view model
+        ///     Loads the audio stations from PVL and injects them into the view model
         /// </summary>
         private async void LoadAudioStations()
         {
-            var stations = await API.Instance.StationFetchTask("audio");
+            Station[] stations = await API.Instance.StationFetchTask("audio");
             var collection = new StationGroup
             {
                 Title = "Radio",
                 Stations = new ObservableCollection<Station>(stations)
             };
-            this.DefaultViewModel["AudioStations"] = collection;
+            DefaultViewModel["AudioStations"] = collection;
+            _radioLoaded = true;
+
+            if (_radioLoaded && _videoLoaded)
+                DataLoaded = true;
         }
 
         private async void LoadVideoStations()
         {
-            var stations = await API.Instance.StationFetchTask("video");
+            Station[] stations = await API.Instance.StationFetchTask("video");
             var collection = new StationGroup
             {
                 Title = "Video",
                 Stations = new ObservableCollection<Station>(stations)
             };
-            this.DefaultViewModel["VideoStations"] = collection;
+            DefaultViewModel["VideoStations"] = collection;
+            _videoLoaded = true;
+
+            if (_radioLoaded && _videoLoaded)
+                DataLoaded = true;
         }
 
         #endregion
@@ -194,7 +197,7 @@ namespace PVL
 
         #region Transport controls
 
-        void SystemControls_ButtonPressed(SystemMediaTransportControls sender,
+        private void SystemControls_ButtonPressed(SystemMediaTransportControls sender,
             SystemMediaTransportControlsButtonPressedEventArgs args)
         {
             switch (args.Button)
@@ -210,12 +213,12 @@ namespace PVL
             }
         }
 
-        async void StartPlayer()
+        private async void StartPlayer()
         {
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => MediaPlayer.Play());
         }
 
-        async void PausePlayer()
+        private async void PausePlayer()
         {
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => MediaPlayer.Pause());
         }
